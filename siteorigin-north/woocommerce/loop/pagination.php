09107f7a3d8d92c1cf0b38ac50417416279bc578
Sean Murphy <sean@iamseanmurphy.com>
2024-10-19 01:14:38 +0000

    1.20.5

---
+++ siteorigin-north/woocommerce/loop/pagination.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Pagination - Show numbered pagination for catalog pages
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ *
+ * @version 9.3.0
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+siteorigin_north_posts_pagination();
