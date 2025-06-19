6e92643b609ee5a2d7d5cae4a6bc6f2e331210df
Sean Murphy <sean@iamseanmurphy.com>
2025-06-19 23:17:15 +0000

    1.20.10

--- siteorigin-north/functions.php
+++ siteorigin-north/functions.php
@@ -4,7 +4,7 @@
  *
  * @license GPL 2.0
  */
-define( 'SITEORIGIN_THEME_VERSION', '1.20.9' );
+define( 'SITEORIGIN_THEME_VERSION', '1.20.10' );
 define( 'SITEORIGIN_THEME_JS_PREFIX', '.min' );
 define( 'SITEORIGIN_THEME_CSS_PREFIX', '.min' );
 
